2381835559cb8935bba220db9bf433db1312fb42
mark.yang <mark.yang@lge.com>
2022-08-17 19:30:07 +0900

    [WIP] series sum

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(void){
-    cout << "hello world!\n";
+int N;//협찬 물품의 수
+int D[100000 + 10];//선호도
+int sol = -30001;//첫번째 방법의 최대 선호도
 
-    return 0;
+void InputData(){
+	cin >> N;
+	for(int i=0;i<N;i++){
+		cin >> D[i];
+	}
+}
+
+void Solve()
+{
+	int sum = 0;
+	for(int i=0; i<N; i++){
+		sum += D[i];
+		if(sum > sol){
+			sol=sum;
+		}
+		if(sum <0){
+			sum=0;
+		}
+	}
+}
+
+
+int main(){
+	InputData();//입력 함수
+	Solve();
+	cout << sol << endl;//출력 
+	return 0;
 }
